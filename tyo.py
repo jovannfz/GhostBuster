129bb97ed6330b7a42eb651b2a3cc02f13a5ffc4
Tyo <tyodzaky92@gmail.com>
2026-06-22 20:08:22 +0700

    menambahkan bagian app controller

--- tyo.py
+++ tyo.py
@@ -1,4 +1,27 @@
-entry poin 
+APP CONTROLLER 
+
+class app(tk.Tk):
+    width = 800
+    height = 600
+
+    def __init__(self):
+        super().__init__()
+        self.title("GhostBusters 👻")
+        self.geometry(f"{self.WIDTH}x{self.HEIGHT}+0+0")
+        self.resizable(False, False)
+        self.configure(bg="#0a0f1e")
+
+        self.current_user = None
+
+        container = tk.Frame(self, bg="#0a0f1e")
+        container.pack(fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1) 
+
+        self.frames = {}
+
+
+BAGIAN ENTRY POINT
 
 if __name__ == "__main__":
     try:
